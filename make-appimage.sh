ffc3680b052cbaa56d0f9f7bed2edd31693bc421
fiftydinar <65243233+fiftydinar@users.noreply.github.com>
2025-12-19 14:06:42 +0100

    Update make-appimage.sh

--- make-appimage.sh
+++ make-appimage.sh
@@ -9,7 +9,7 @@ export OUTPATH=./dist
 export ADD_HOOKS="self-updater.bg.hook:fix-namespaces.hook"
 export UPINFO="gh-releases-zsync|${GITHUB_REPOSITORY%/*}|${GITHUB_REPOSITORY#*/}|latest|*$ARCH.AppImage.zsync"
 export ICON=/usr/share/icons/hicolor/scalable/apps/viber.svg
-export DESKTOP=/usr/share/applications/viber.desktop
+export DESKTOP=/usr/share/applications/com.viber.Viber.desktop
 export DEPLOY_PIPEWIRE=1
 export DEPLOY_GSTREAMER=1
 export DEPLOY_OPENGL=1
@@ -22,7 +22,7 @@ export DEPLOY_QT_WEB_ENGINE=0
 # Deploy dependencies
 quick-sharun ./AppDir/bin/*
 
-sed -e 's|Exec=viber|Exec=Viber|g' -i ./AppDir/viber.desktop
+sed -e 's|Exec=viber|Exec=Viber|g' -i ./AppDir/com.viber.Viber.desktop
 
 # Turn AppDir into AppImage
 quick-sharun --make-appimage
