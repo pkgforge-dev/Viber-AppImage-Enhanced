58c867816a13ac03f23f4b3f2aaeb6d11cbea7b6
fiftydinar <65243233+fiftydinar@users.noreply.github.com>
2025-12-19 14:35:04 +0100

    Update get-dependencies.sh

--- get-dependencies.sh
+++ get-dependencies.sh
@@ -11,6 +11,8 @@ make-aur-package viber
 echo "Installing debloated packages..."
 echo "---------------------------------------------------------------"
 get-debloated-pkgs --add-common --prefer-nano
+# Remove qt6-base-mini, as it breaks Viber and Viber already bundles it's own qt6
+pacman -S --noconfirm qt6-base
 
 mkdir -p ./AppDir/bin
 cp -rv /opt/viber/*               ./AppDir/bin
