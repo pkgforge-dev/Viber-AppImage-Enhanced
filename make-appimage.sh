397f83ed28d1fe7d159d20933ee4fef70c7da43e
fiftydinar <65243233+fiftydinar@users.noreply.github.com>
2025-12-19 13:57:42 +0100

    Update make-appimage.sh

--- make-appimage.sh
+++ make-appimage.sh
@@ -14,8 +14,10 @@ export DEPLOY_PIPEWIRE=1
 export DEPLOY_GSTREAMER=1
 export DEPLOY_OPENGL=1
 export DEPLOY_VULKAN=1
+# Viber already bundles those libs already
 export DEPLOY_QT=0
 export DEPLOY_QML=0
+export DEPLOY_QT_WEB_ENGINE=0
 
 # Deploy dependencies
 quick-sharun ./AppDir/bin/*
