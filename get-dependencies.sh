3c46bb15084a120a04d33a78f0c5dd8f7041d7aa
fiftydinar <65243233+fiftydinar@users.noreply.github.com>
2025-12-21 22:14:07 +0100

    Install `ffmpeg-mini`

--- get-dependencies.sh
+++ get-dependencies.sh
@@ -11,7 +11,7 @@ make-aur-package viber
 echo "Installing debloated packages..."
 echo "---------------------------------------------------------------"
 # Don't install qt6-base, as it breaks Viber and Viber already bundles it's own qt6
-get-debloated-pkgs --add-common --prefer-nano ! qt6-base
+get-debloated-pkgs --add-common --prefer-nano ffmpeg-mini ! qt6-base
 
 mkdir -p ./AppDir/bin
 cp -rv /opt/viber/*               ./AppDir/bin
