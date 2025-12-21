bafbfc424871d88ac701c66a20129123f31af2b9
fiftydinar <65243233+fiftydinar@users.noreply.github.com>
2025-12-22 00:32:52 +0100

    Install `intel` driver for it's GPU acceleration

--- get-dependencies.sh
+++ get-dependencies.sh
@@ -10,10 +10,14 @@ make-aur-package libtiff5
 pacman -Syu --noconfirm libtheora libbluray ffmpeg
 make-aur-package viber
 
+if [ "$ARCH" = 'x86_64' ]; then
+	pacman -Syu --noconfirm libva-intel-driver
+fi
+
 echo "Installing debloated packages..."
 echo "---------------------------------------------------------------"
 # Don't install qt6-base, as it breaks Viber and Viber already bundles it's own qt6
-get-debloated-pkgs --add-common --prefer-nano ! qt6-base
+get-debloated-pkgs --add-common --prefer-nano intel-media-driver-mini ! qt6-base
 
 mkdir -p ./AppDir/bin
 cp -rv /opt/viber/*               ./AppDir/bin
