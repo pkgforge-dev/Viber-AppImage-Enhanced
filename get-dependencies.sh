289fa2c033e1510370ab8140d5a0062ce8522783
fiftydinar <65243233+fiftydinar@users.noreply.github.com>
2026-01-20 19:42:48 +0100

    Update get-dependencies.sh

--- get-dependencies.sh
+++ get-dependencies.sh
@@ -4,21 +4,20 @@ set -eu
 
 ARCH=$(uname -m)
 
-echo "Installing package dependencies..."
-echo "---------------------------------------------------------------"
-make-aur-package libtiff5
-make-aur-package libjpeg6-turbo
-make-aur-package ffmpeg7.1
-make-aur-package
-
-if [ "$ARCH" = 'x86_64' ]; then
-	pacman -Syu --noconfirm libva-intel-driver
-fi
-
 echo "Installing debloated packages..."
 echo "---------------------------------------------------------------"
 # Don't install qt6-base, as it breaks Viber and Viber already bundles it's own qt6
 get-debloated-pkgs --add-common --prefer-nano intel-media-driver-mini ! qt6-base
+
+echo "Installing package dependencies..."
+echo "---------------------------------------------------------------"
+if [ "$ARCH" = 'x86_64' ]; then
+	pacman -Syu --noconfirm libva-intel-driver
+fi
+make-aur-package libtiff5
+make-aur-package libjpeg6-turbo
+make-aur-package ffmpeg7.1
+make-aur-package
 
 mkdir -p ./AppDir/bin
 cp -rv /opt/viber/*               ./AppDir/bin
