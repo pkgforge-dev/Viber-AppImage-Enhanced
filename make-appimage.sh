9945419c981b220355302be6ffd520395534d1c4
fiftydinar <65243233+fiftydinar@users.noreply.github.com>
2026-04-06 14:26:49 +0200

    `FEX` integration with `aarch64` AppImages is not ready yet

--- make-appimage.sh
+++ make-appimage.sh
@@ -46,8 +46,5 @@ sed -i '/Path=/d' ./AppDir/com.viber.Viber.desktop
 # Turn AppDir into AppImage
 quick-sharun --make-appimage
 
-# Also make an aarch64 AppImage that uses FEX
-quick-sharun --make-aarch64-appimage
-
 # Test the final app
-quick-sharun --test ./dist/*x86_64.AppImage
+quick-sharun --test ./dist/*.AppImage
